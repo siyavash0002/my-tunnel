0305f64cdf08c42bbe4a22776ac03c24cb0e601f
siyavash0002 <siyavash0002@gmail.com>
2026-05-10 04:19:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const net = require('net');
-const http = require('http');
 
-const server = http.createServer((req, res) => {
+module.exports = (req, res) => {
     const dest = process.env.DEST;
     const path = process.env.V_PATH || '/vercel-xhttp';
 
-    if (req.url !== path) {
-        res.writeHead(404);
-        res.end('Not Found');
+    if (req.url.split('?')[0] !== path) {
+        res.statusCode = 404;
+        res.end();
         return;
     }
 
     const [destIp, destPort] = dest.split(':');
-    const client = new net.Socket();
-
-    client.connect(destPort, destIp, () => {
+    const client = net.connect(destPort, destIp, () => {
         req.pipe(client).pipe(res);
     });
 
     client.on('error', () => {
-        if (!res.headersSent) {
-            res.writeHead(502);
-            res.end();
-        }
+        res.statusCode = 502;
+        res.end();
     });
-});
-
-export default server;
+};
